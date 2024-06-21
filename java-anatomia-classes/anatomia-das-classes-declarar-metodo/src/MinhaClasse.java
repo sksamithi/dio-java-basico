6febd63998bab09500d6176f72f0269a3603d549
sksamithi <vieira.thaism@gmail.com>
2024-06-21 14:24:06 -0300

    atualizacao

--- java-anatomia-classes/anatomia-das-classes-declarar-metodo/src/MinhaClasse.java
+++ java-anatomia-classes/anatomia-das-classes-declarar-metodo/src/MinhaClasse.java
@@ -1,17 +1,19 @@
 public class MinhaClasse {
     
     public static void main (String [] args){
-
+        
+        //Declaração de variáveis:
         String primeiroNome = "Thais";
         String segundoNome = "Macedo Vieira";
-
+        //Faz-se uso do método nomeCompleto definido abaixo do método main:
         String nomeCompleto = nomeCompleto (primeiroNome, segundoNome);
-
+        //Imprime no terminal o resultado da aplicação do método:
         System.out.println(nomeCompleto);
 
     }
-
+    //Declaração de um novo método chamado de nomeCompleto
     public static String nomeCompleto (String primeiroNome, String segundoNome) {
+        //Retorna a concatenação das strings (variáveis):
         return primeiroNome.concat(" ").concat(segundoNome);
     }
 }
